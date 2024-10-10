e81bac75adcf5d62581c6b4b650c9bda3c08217e
Anjan Diyora <adproduction50@gmail.com>
2024-10-10 02:50:54 -0400

    feat: Add task3.sh script for Titanic data analysis

---
+++ task3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tail -n +2 titanic.csv | \
+gawk 'BEGIN {FS=","; OFS=","} {for (i=1; i<=NF; i++) if ($i == "") $i = "NULL"; print}' | \
+gawk -F',' '$3 == 2 && $13 ~ /S/ { print }' | \
+sed -e 's/male/M/' -e 's/female/F/' | \
+gawk -F',' '$7 != "NULL" { age_sum += $7; age_count++ } END { if (age_count > 0) print "Average Age:", age_sum / age_count; }'
