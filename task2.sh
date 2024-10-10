3903ecb462465794af0adbe15027f92df69c15f8
Anjan Diyora <adproduction50@gmail.com>
2024-10-10 02:44:22 -0400

    feat: Add filtering for low occurrence files in task2.sh

--- task2.sh
+++ task2.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-set -x  
+#set -x  
 
-grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | awk '$1 >= 3 { print $2 }' | while read -r file; do
-    echo "$file"
-done | sort -k1,1nr | sed 's/file_/filtered_/'
+grep -l "sample" file* | 
+xargs grep -o "CSC510" | 
+uniq -c | 
+cut -d: -f1 | 
+awk '$1 >= 3 { print $2 }' | 
+sed 's/^ *//' | 
+gawk '{count=$1; file=$2; "ls -l " file | getline size; print count, size}' | 
+sort -k1,1nr -k6,6nr | 
+awk '{print $10}' | 
+sed 's/file_/filtered_/'
+
+
+
